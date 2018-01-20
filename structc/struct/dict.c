d645d78db3412672e7153857c06e614694897e25
wangzhi <wangzhi@kingsoft.com>
2018-01-20 14:09:08 +0800

    提交dict操作

--- structc/struct/dict.c
+++ structc/struct/dict.c
@@ -1,36 +1,5 @@
 ﻿#include <dict.h>
-
-//
-// 质数表
-//
-static const unsigned _primes[][2] = {
-    { (2^6 )-1,         53 },
-    { (2^7 )-1,         97 },
-    { (2^8 )-1,        193 },
-    { (2^9 )-1,        389 },
-    { (2^10)-1,        769 },
-    { (2^11)-1,       1543 },
-    { (2^12)-1,       3079 },
-    { (2^13)-1,       6151 },
-    { (2^14)-1,      12289 },
-    { (2^15)-1,      24593 },
-    { (2^16)-1,      49157 },
-    { (2^17)-1,      98317 },
-    { (2^18)-1,     196613 },
-    { (2^19)-1,     393241 },
-    { (2^20)-1,     786433 },
-    { (2^21)-1,    1572869 },
-    { (2^22)-1,    3145739 },
-    { (2^23)-1,    6291469 },
-    { (2^24)-1,   12582917 },
-    { (2^25)-1,   25165843 },
-    { (2^26)-1,   50331653 },
-    { (2^27)-1,  100663319 },
-    { (2^28)-1,  201326611 },
-    { (2^29)-1,  402653189 },
-    { (2^30)-1,  805306457 },
-    { UINT_MAX, 1610612741 },
-};
+#include <assert.h>
 
 struct keypair {
     struct keypair * next;
@@ -56,9 +25,41 @@ static inline struct keypair * _keypair_create(unsigned hash, void * v, const ch
     return pair;
 }
 
+//
+// 质数表
+//
+static const unsigned _primes[][2] = {
+    { (1<<6)-1  ,         53 },
+    { (1<<7)-1  ,         97 },
+    { (1<<8)-1  ,        193 },
+    { (1<<9)-1  ,        389 },
+    { (1<<10)-1 ,        769 },
+    { (1<<11)-1 ,       1543 },
+    { (1<<12)-1 ,       3079 },
+    { (1<<13)-1 ,       6151 },
+    { (1<<14)-1 ,      12289 },
+    { (1<<15)-1 ,      24593 },
+    { (1<<16)-1 ,      49157 },
+    { (1<<17)-1 ,      98317 },
+    { (1<<18)-1 ,     196613 },
+    { (1<<19)-1 ,     393241 },
+    { (1<<20)-1 ,     786433 },
+    { (1<<21)-1 ,    1572869 },
+    { (1<<22)-1 ,    3145739 },
+    { (1<<23)-1 ,    6291469 },
+    { (1<<24)-1 ,   12582917 },
+    { (1<<25)-1 ,   25165843 },
+    { (1<<26)-1 ,   50331653 },
+    { (1<<27)-1 ,  100663319 },
+    { (1<<28)-1 ,  201326611 },
+    { (1<<29)-1 ,  402653189 },
+    { (1<<30)-1 ,  805306457 },
+    { UINT_MAX  , 1610612741 },
+};
+
 struct dict {
     node_f fdie;                // 结点注册的销毁函数
-    unsigned idx;               // 使用的质数表索引
+    unsigned idx;               // 使用 _primes 质数表索引
     unsigned used;              // 用户已经使用的结点个数
     struct keypair ** table;    // size = primes[idx][0]
 };
@@ -92,4 +93,128 @@ static void _dict_resize(struct dict * d) {
     // table 重新变化
     free(d->table);
     d->table = table;
+}
+
+//
+// dict_create - 字典创建
+// fdie		: v 销毁函数
+// return	: dict_t
+//
+inline dict_t 
+dict_create(node_f fdie) {
+    struct dict * d = malloc(sizeof(struct dict));
+    unsigned size = _primes[d->idx = 0][1];
+    d->used = 0;
+    d->fdie = fdie;
+    // 默认构建的第一个素数表 index = 0
+    d->table = calloc(size, sizeof(struct keypair *));
+    return d;
+}
+
+//
+// dict_delete - 字典销毁
+// d		: dict_create 创建的字典对象
+// return   : void 
+//
+void 
+dict_delete(dict_t d) {
+    unsigned i, size;
+    if (NULL == d) return;
+    size = _primes[d->idx][1];
+    for (i = 0; i < size; ++i) {
+        struct keypair * pair = d->table[i];
+        while (pair) {
+            struct keypair * next = pair->next;
+            _keypair_delete(d->fdie, pair);
+            pair = next;
+        }
+    }
+    free(d->table);
+    free(d);
+}
+
+//
+// dict_set - 设置一个 <k, v> 结构
+// d		: dict_create 创建的字典对象
+// k		: 插入的 key
+// v		: 插入数据的值, NULL 会销毁 k
+// return	: void
+//
+void 
+dict_set(dict_t d, const char * k, void * v) {
+    unsigned hash, index;
+    struct keypair * pair, * prev;
+    assert(NULL != d && k != NULL);
+
+    // 检查一下内存, 看是否要扩充
+    _dict_resize(d);
+
+    // 开始寻找数据
+    hash = str_hash(k);
+    index = hash % _primes[d->idx][1];
+    pair = d->table[index];
+    prev = NULL;
+
+    while (pair) {
+        // 找见了数据
+        if (pair->hash == hash && !strcmp(pair->key, k)) {
+            // 相同数据直接返回什么都不操作
+            if (pair->val == v)
+                return;
+
+            // 删除操作
+            if (NULL == v) {
+                if (NULL == prev)
+                    d->table[index] = pair->next;
+                else
+                    prev->next = pair->next;
+
+                // 销毁结点, 直接返回
+                _keypair_delete(d->fdie, pair);
+                return;
+            }
+
+            // 更新结点
+            if (d->fdie)
+                d->fdie(pair->val);
+            pair->val = v;
+            return;
+        }
+
+        prev = pair;
+        pair = pair->next;
+    }
+
+    // 没有找见设置操作, 直接插入数据
+    if (NULL != v) {
+        pair = _keypair_create(hash, v, k);
+        pair->next = d->table[index];
+        d->table[index] = pair;
+        ++d->used;
+    }
+}
+
+//
+// dict_get - 获取字典中对映的 v
+// d		: dict_create 创建的字典对象
+// k		: 查找的 key 
+// return   : 查找的 v, NULL 表示没有
+//
+void * 
+dict_get(dict_t d, const char * k) {
+    unsigned hash, index;
+    struct keypair * pair;
+    assert(NULL != d && k != NULL);
+
+    hash = str_hash(k);
+    index = hash % _primes[d->idx][1];
+    pair = d->table[index];
+
+    while (pair) {
+        if (!strcmp(pair->key, k))
+            return pair->val;
+        pair = pair->next;
+    }
+
+    return NULL;
 }
