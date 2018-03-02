d29563f79026af3543e5d8fb5cf07c35f8a8ca5b
wangzhi <wangzhi@kingsoft.com>
2018-03-03 03:31:28 +0800

    提交 rand 函数操作

--- structc/system/rand.c
+++ structc/system/rand.c
@@ -1,3 +1,70 @@
-#include <rand.h>
+﻿#include <rand.h>
+#include <time.h>
+#include <assert.h>
 
+#define CARRY(x, y)     ((x + y) > MASK) // 二者和是否进位, 基于 16位
+#define ADDRQ(x, y, z)  (z = CARRY(x, y), x = LOW(x + y))
 
+#define MUL(x, y, z)    l = (x) * (y); z[0] = LOW(l); z[1] = HIGH(l)
+
+static void _rand_next(rand_t r) {
+    uint32_t l;
+    uint32_t p[2], q[2], s[2], c[2];
+
+    MUL(r->a[0], r->x[0], p);
+    ADDRQ(p[0], r->c, c[0]);
+    ADDRQ(p[1], c[0], c[1]);
+    MUL(r->a[0], r->x[1], q);
+    ADDRQ(p[1], q[0], c[0]);
+    MUL(r->a[1], r->x[0], s);
+
+    l = c[0] + c[1] + CARRY(p[1], s[0]) + q[1] + s[1]
+        + r->a[0] * r->x[2] + r->a[1] * r->x[1] + r->a[2] * r->x[0];
+    r->x[2] = LOW(l);
+    r->x[1] = LOW(p[1] + s[0]);
+    r->x[0] = LOW(p[0]);
+}
+
+//
+// rand_rand - 获取一个随机值
+// r        : 随机函数对象
+// return   : 返回生成的随机函数
+//
+inline int32_t 
+rand_rand(rand_t r) {
+    _rand_next(r);
+    return (r->x[2] << (N - 1)) + (r->x[1] >> 1);
+}
+
+//
+// 我 - 想赢, 因为不甘心
+//
+static rand_t _r = { { { X0, X1, X2 }, { A0, A1, A2 }, C } };
+
+// EXTERN_RUN(r_init) 启动初始化
+extern inline void r_init(void) {
+    rand_init(_r, time(NULL));
+}
+
+//
+// r_rand  - 得到 [0, INT32_MAX] 随机数
+// r_randk - 得到一个 64 位的 key
+// r_rands - 得到 [min, max] 范围内随机数
+//
+inline int32_t 
+r_rand(void) {
+    return rand_rand(_r);
+}
+
+inline int64_t 
+r_randk(void) {
+    uint64_t x = ((r_rand() << N) ^ r_rand()) & INT32_MAX;
+    uint64_t y = ((r_rand() << N) ^ r_rand()) & INT32_MAX;
+    return ((x << 2 * N) | y) & INT64_MAX;
+}
+
+inline int32_t 
+r_rands(int32_t min, int32_t max) {
+    assert(max > min);
+    return r_rand() % (max - min + 1) + min;
+}
--- structc/system/rand.h
+++ structc/system/rand.h
@@ -5,6 +5,55 @@
 // 线程安全的跨平台的 rand 库
 //
 
- 
+#include <stdint.h>
+
+struct rand {
+    uint32_t x[3];
+    uint32_t a[3];
+    uint32_t c;
+};
+
+typedef struct rand rand_t[1];
+
+#define X0	            (0x330E)
+#define X1	            (0xABCD)
+#define X2	            (0x1234)
+#define A0	            (0xE66D)
+#define A1	            (0xDEEC)
+#define A2	            (0x0005)
+#define C	            (0x000B)
+
+#define N               (16)
+#define MASK            ((1 << N) - 1)
+#define LOW(x)          ((unsigned)(x) & MASK)
+#define HIGH(x)         LOW((x) >> N)
+
+//
+// rand_init - 随机函数初始化种子方法
+// r        : 随机函数对象
+// seed     : 种子数
+// return   : void
+//
+inline void rand_init(rand_t r, int64_t seed) {
+    r->x[0] = X0; r->x[1] = LOW(seed); r->x[2] = HIGH(seed);
+    r->a[0] = A0; r->a[1] = A1; r->a[2] = A2;
+    r->c = C;
+}
+
+//
+// rand_rand - 获取一个随机值
+// r        : 随机函数对象
+// return   : 返回生成的随机函数
+//
+extern int32_t rand_rand(rand_t r);
+
+//
+// r_rand  - 得到 [0, INT32_MAX] 随机数
+// r_randk - 得到一个 64 位的 key
+// r_rands - 得到 [min, max] 范围内随机数
+//
+extern int32_t r_rand(void);
+extern int64_t r_randk(void);
+extern int32_t r_rands(int32_t min, int32_t max);
 
 #endif//_H_RAND
