1b3954507bcf3838cb2321c76cb9ed1ea93bac34
WANGZHI <wangzhi@kingsoft.com>
2018-05-11 09:35:41 +0800

    修改接口定义

--- structc/system/pipe.h
+++ structc/system/pipe.h
@@ -12,22 +12,26 @@
 // 
 extern int pipe(socket_t pipefd[2]);
 
-//
 // pipe_t - 通道类型
 // pipe_close - 通道关闭操作
-//
-typedef HANDLE pipe_t;
 #define pipe_close      CloseHandle
+typedef HANDLE pipe_t;
 
 #else
 
-typedef int pipe_t;
 #define pipe_close      close
+typedef int pipe_t;
 
 #endif
 
 // chan_t - 管道类型
 typedef struct { pipe_t recv, send; } chan_t[1];
+
+// chan_close - 管道关闭
+inline void chan_close(chan_t ch) {
+    pipe_close(ch->recv);
+    pipe_close(ch->send);
+}
 
 //
 // chan_open - 打开一个管道
@@ -36,15 +40,9 @@ typedef struct { pipe_t recv, send; } chan_t[1];
 //
 extern int chan_open(chan_t ch);
 
-// chan_close - 管道关闭
-inline void chan_close(chan_t ch) {
-    pipe_close(ch->recv);
-    pipe_close(ch->send);
-}
-
-// chan_recv - 管道阻塞接收, 在 PIPE_BUF 4K 内原子交换
+// chan_recv - 管道阻塞接收, PIPE_SIZE 4K 内原子交换
 // chan_send - 管道阻塞发送, 
-extern int chan_recv(chan_t ch, void * data, size_t len);
-extern int chan_send(chan_t ch, const void * data, size_t len);
+extern int chan_recv(chan_t ch, void * buf, int sz);
+extern int chan_send(chan_t ch, const void * buf, int sz);
 
 #endif//_H_PIPE
