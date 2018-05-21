ccc884fe9a7e6c396ef95e01ecf6cdacc394dc0f
wangzhi <wangzhi@kingsoft.com>
2018-05-22 01:08:34 +0800

    布局调整

---
+++ structc/system/rwlock.c
@@ -0,0 +1,28 @@
+﻿#include <rwlock.h>
+
+// add read lock
+void 
+rwlock_rlock(struct rwlock * lock) {
+    for (;;) {
+        // 看是否有人在试图读, 得到并防止代码位置优化
+        while (lock->wlock)
+            atom_sync();
+
+        ATOM_INC(lock->rlock);
+        // 没有写占用, 开始读了
+        if (!lock->wlock)
+            break;
+
+        // 还是有写, 删掉添加的读
+        ATOM_DEC(lock->rlock);
+    }
+}
+
+// add write lock
+void 
+rwlock_wlock(struct rwlock * lock) {
+    atom_lock(lock->wlock);
+    // 等待读占用锁
+    while (lock->rlock)
+        atom_sync();
+}
--- structc/system/rwlock.h
+++ structc/system/rwlock.h
@@ -24,28 +24,9 @@ inline void rwlock_unrlock(struct rwlock * lock) {
 }
 
 // add read lock
-inline void rwlock_rlock(struct rwlock * lock) {
-    for (;;) {
-        // 看是否有人在试图读, 得到并防止代码位置优化
-        while (lock->wlock)
-            atom_sync();
-
-        ATOM_INC(lock->rlock);
-        // 没有写占用, 开始读了
-        if (!lock->wlock)
-            break;
-
-        // 还是有写, 删掉添加的读
-        ATOM_DEC(lock->rlock);
-    }
-}
+extern void rwlock_rlock(struct rwlock * lock);
 
 // add write lock
-inline void rwlock_wlock(struct rwlock * lock) {
-    atom_lock(lock->wlock);
-    // 等待读占用锁
-    while (lock->rlock)
-        atom_sync();
-}
+extern void rwlock_wlock(struct rwlock * lock);
 
 #endif//_H_RWLOCK
