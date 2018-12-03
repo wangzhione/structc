1bc5b44ee71cb2a081d8cf535e5c7631c4fdc945
wangzhi <wangzhione@163.com>
2018-12-03 19:43:22 +0800

    thread test volatile

--- structc/test/thread_test.c
+++ structc/test/thread_test.c
@@ -1,18 +1,28 @@
-﻿#include <thread.h>
+﻿#include <head.h>
 
-static void heoo(int cnt) {
-    for (int i = 0; i < cnt; ++i) {
-        printf("%s -> %d\n", __func__, i);
-    }
+volatile static int c;
+
+static void heoo(int index) {
+    for (int i = 0; i < 10000; ++i)
+        ++c;
+    printf("index = %d, g = %d\n", index, c);
 }
 
 //
 // test thread demo
 //
 void thread_test(void) {
-    pthread_t tid;
-    if (pthread_run(tid, heoo, __LINE__)) {
-        RETNIL("pthread_run is error!");
-    }
-    pthread_end(tid);
+    int i, j;
+    pthread_t ids[10];
+
+    // 开始运行
+    for (i = 0; i < LEN(ids); ++i)
+        if (pthread_run(ids[i], heoo, i))
+            RETNIL("pthread_run is error!");
+
+    // 等待结束
+    for (j = 0; j < i; ++j)
+        pthread_end(ids[j]);
+
+    printf("g = %d\n", c);
 }
