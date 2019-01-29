e6362d5322681057d41e9063bcbafa54569366be
wangzhi <wangzhione@163.com>
2019-01-30 00:23:05 +0800

    msg buf

---
+++ structc/base/buf.c
@@ -0,0 +1,69 @@
+﻿#include "buf.h"
+
+#define BUF_INT         (1024)
+
+//
+// msg buffer manager
+//
+struct msg_buf {
+    uint32_t sz;    // q.sz is msg header body size length
+    char * data;    // 数据流
+    int cap;        // 容量
+    int len;        // 长度
+};
+
+// msg_buf_expand - 内存扩充 
+static void msg_buf_expand(struct msg_buf * q, int sz) {
+    // 确定内存是否足够
+    int cap = q->cap, len = q->len;
+    if (len + sz <= cap)
+        return;
+
+    // 开始构建所需内存
+    do cap <<= 1; while (len + sz > cap);
+    q->data = realloc(q->data, cap);
+    assert(q->data != NULL);
+    q->cap = cap;
+}
+
+//
+// msg_buf_create - msg buffer 创建
+// return   : msg buffer
+//
+inline msg_buf_t 
+msg_buf_create(void) {
+    struct msg_buf * q = malloc(sizeof(struct msg_buf));
+    q->sz = 0;
+    q->data = malloc(q->cap = BUF_INT);
+    q->len = 0;
+    return q;
+}
+
+//
+// msg_buf_delete - msg buffer 删除
+// q        : msg buffer
+// return   : void
+//
+inline void 
+msg_buf_delete(msg_buf_t q) {
+    if (q) {
+        free(q->data);
+        free(q);
+    }
+}
+
+//
+// buf_append - msg buffer 添加数据, 并尝试解析出结果
+// q        : msg buffer
+// p        : return msg
+// data     : 内存数据
+// sz       : 内存数据 size
+// return   : EParse 协议解析错误, ESmall 协议不完整
+//
+int 
+msg_buf_append(msg_buf_t q, msg_t * p, 
+               const void * data, uint32_t sz) {
+    
+
+    return SBase;
+}
---
+++ structc/base/buf.h
@@ -0,0 +1,36 @@
+﻿#ifndef _BUF_H
+#define _BUF_H
+
+#include "msg.h"
+
+//
+// buffer recv send msg
+//
+typedef struct msg_buf * msg_buf_t;
+
+//
+// msg_buf_create - msg buffer 创建
+// return   : msg buffer
+//
+extern msg_buf_t msg_buf_create(void);
+
+//
+// msg_buf_delete - msg buffer 删除
+// q        : msg buffer
+// return   : void
+//
+extern void msg_buf_delete(msg_buf_t q);
+
+//
+// buf_append - msg buffer 添加数据, 并尝试解析出结果
+// q        : msg buffer
+// p        : return msg
+// data     : 内存数据
+// sz       : 内存数据 size
+// return   : EParse 协议解析错误, ESmall 协议不完整
+//
+extern int msg_buf_append(msg_buf_t q, msg_t * p,
+                          const void * data, uint32_t sz);
+
+#endif//_BUF_H
+
