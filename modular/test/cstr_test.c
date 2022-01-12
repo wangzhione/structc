9f2b8684db36877f8d25061729adf55bfbe7b372
Zhi Wang <wangzhi5@xiaomi.com>
2022-01-12 16:12:39 +0800

    增加注释

--- modular/test/cstr_test.c
+++ modular/test/cstr_test.c
@@ -13,15 +13,22 @@ void cstr_test(void) {
         struct cstr str[1];
     };
 
+    // 初始化
     struct astr a = { ATOMIC_FLAG_INIT };
 
+    // 加锁
     atomic_flag_lock(&a.lock);
 
+    // 使用
+    // 各种对于 astr.str 操作都是线程安全的
+    // ...
     cstr_appends(a.str, "100");
 
     printf("str = %s, cap = %zu, len = %zu\n", cstr_get(a.str), a.str->cap, a.str->len);
 
+    // 释放锁
     atomic_flag_unlock(&a.lock);
 
+    // 销毁
     cstr_free(a.str);
 }
