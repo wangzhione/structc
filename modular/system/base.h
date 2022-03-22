18d975010e37c1a17d64107f174b11d782dbf0f4
Zhi Wang <wangzhi5@xiaomi.com>
2022-03-22 19:21:44 +0800

    base upgrade

--- modular/system/base.h
+++ modular/system/base.h
@@ -10,8 +10,10 @@
 // STR - 添加双引号的宏 
 // v        : 变量标识
 //
+#ifndef STR
 #define STR(v)  S_R(v)
 #define S_R(v)  #v
+#endif
 
 #ifndef LEN
 //
@@ -20,18 +22,6 @@
 //
 #define LEN(a)  ((int)(sizeof(a) / sizeof(*(a))))
 #endif
-
-//
-// CODE_RUN - 代码块测试, 并输出运行时间
-// code     : { ... } 包裹的代码块
-//
-#define CODE_RUN(code)                                 \
-do {                                                   \
-    clock_t $s = clock();                              \
-    code                                               \
-    double $e = (double)clock();                       \
-    printf("code run %lfs\n", ($e-$s)/CLOCKS_PER_SEC); \
-} while (0)
 
 //
 // EXTERN_RUN - 函数包装宏, 声明并立即使用
